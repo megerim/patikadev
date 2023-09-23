ef9241df20a7fd1b31ea45008c9a31392c95b96f
Mehmet Erim Gökhan <97033161+megerim@users.noreply.github.com>
2023-09-23 23:52:13 +0300

    node4

---
+++ nodeJS/node4.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+
+// fs.writeFile('employees.json', '{"name": "Employee 1 Name", "salary": 2000}', (err) => {
+//     if (err) throw err;
+//     console.log('Dosya oluşturuldu.');
+// });
+
+// fs.readFile('employees.json', 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+// fs.appendFile('employees.json', '\n{"name": "Employee 2 Name", "salary": 3000}', (err) => {
+//     if (err) throw err;
+//     console.log('Dosya güncellendi.');
+// });
+
+
+// fs.unlink('employees.json', (err) => {
+//     if (err) throw err;
+//     console.log('Dosya silindi.');
+// });
